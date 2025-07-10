f03c0b2796605e99f75677d8bacfc5ce35fb0a11
r4veeen <bas.rojasl@duocuc.cl>
2025-07-10 17:59:45 -0400

    analizar personalizacion y hooks de features

--- src/hooks/useFeatures.ts
+++ src/hooks/useFeatures.ts
@@ -1,11 +1,10 @@
-// src/hooks/useFeatures.ts
 import { useEffect } from "react";
 import { useGetFeaturesQuery } from "@/services/RtkQueryService";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFeaturesStatus } from "@/store/slices/featuresSlice/featuresSlice";
 import { AppDispatch } from "@/store/storeSetup";
 
-// Define FeatureStatus enum
+
 export enum FeatureStatus {
   Loading = "loading",
   Failed = "failed",
@@ -17,12 +16,11 @@ export default function useFeatures() {
   const { isLoading, isError, data } = useGetFeaturesQuery(undefined);
   const status = useSelector(selectFeaturesStatus);
 
-  // (opcional) si quieres despachar algo extra:
-  useEffect(() => {
-    if (data) {
-      // por ejemplo guardar en otro slice… o nada
-    }
-  }, [data, dispatch]);
+  // useEffect(() => {
+  //   if (data) {
+      
+  //   }
+  // }, [data, dispatch]);
 
   if (isLoading) return FeatureStatus.Loading;
   if (isError) return FeatureStatus.Failed;
