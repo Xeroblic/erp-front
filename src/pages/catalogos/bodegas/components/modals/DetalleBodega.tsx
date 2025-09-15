e0d73d80806faca1a3173d1773737ac0ebebdbd1
r4veeen <bas.rojasl@duocuc.cl>
2025-09-15 17:56:14 -0300

    arreglo de modal crearbodega

---
+++ src/pages/catalogos/bodegas/components/modals/DetalleBodega.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetalleBodega = () => {
+  return (
+    <div>DetalleBodega</div>
+  )
+}
+
+export default DetalleBodega
---
+++ src/pages/catalogos/bodegas/components/modals/EliminarBodega.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EliminarBodega = () => {
+  return (
+    <div>EliminarBodega</div>
+  )
+}
+
+export default EliminarBodega
