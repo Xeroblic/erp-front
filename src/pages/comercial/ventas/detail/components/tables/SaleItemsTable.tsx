4ed25d40b1bb0c68b0cfb42e7f1a20520c7d4b55
r4veeen <bas.rojasl@duocuc.cl>
2025-12-16 15:39:16 -0300

    feat: formatear valores monetarios en la tabla de artículos de venta

--- src/pages/comercial/ventas/detail/components/tables/SaleItemsTable.tsx
+++ src/pages/comercial/ventas/detail/components/tables/SaleItemsTable.tsx
@@ -3,6 +3,7 @@ import { ColumnDef } from '@tanstack/react-table';
 import DataTable from '@/components/ui/DataTable';
 import { ISaleItem } from '@/interface/sales.interface';
 import { formatCLP } from '../../../utils';
+import { priceFormatWhitDecimals } from '@/utils/priceFormat.util';
 
 interface SaleItemsTableProps {
 	items: ISaleItem[];
@@ -64,48 +65,48 @@ const SaleItemsTable: React.FC<SaleItemsTableProps> = ({ items }) => {
 					);
 					return (
 						<span className='text-sm text-zinc-700 dark:text-zinc-300'>
-							{unitPrice}
+							{priceFormatWhitDecimals(unitPrice)}
 						</span>
 					);
 				},
 				meta: { align: 'right' },
 				size: 120,
 			},
-			// {
-			// 	accessorKey: 'tax_amount',
-			// 	header: 'IVA',
-			// 	cell: ({ row }) => {
-			// 		const taxAmount = Number(
-			// 			row.original.tax_amount ??
-			// 				0,
-			// 		);
-			// 		return (
-			// 			<span className='text-sm text-zinc-700 dark:text-zinc-300'>
-			// 				{formatCLP(taxAmount)}
-			// 			</span>
-			// 		);
-			// 	}
+			{
+				accessorKey: 'tax_amount',
+				header: 'IVA',
+				cell: ({ row }) => {
+					const taxAmount = Number(
+						row.original.tax_amount ??
+							0,
+					);
+					return (
+						<span className='text-sm text-zinc-700 dark:text-zinc-300'>
+							{priceFormatWhitDecimals(taxAmount)}
+						</span>
+					);
+				}
 
-			// },
-			// {
-			// 	accessorKey: 'total',
-			// 	header: 'Total',
-			// 	cell: ({ row }) => {
-			// 		const unitPrice = Number(
-			// 			row.original.unit_price ?? row.original.unit_price_gross ?? 0,
-			// 		);
-			// 		const totalLine = Number(
-			// 			row.original.total ?? row.original.subtotal ?? row.original.quantity * unitPrice,
-			// 		);
-			// 		return (
-			// 			<span className='text-sm font-semibold text-zinc-900 dark:text-zinc-100'>
-			// 				{formatCLP(totalLine)}
-			// 			</span>
-			// 		);
-			// 	},
-			// 	meta: { align: 'right' },
-			// 	size: 120,
-			// },
+			},
+			{
+				accessorKey: 'total',
+				header: 'Total',
+				cell: ({ row }) => {
+					const unitPrice = Number(
+						row.original.unit_price ?? row.original.unit_price_gross ?? 0,
+					);
+					const totalLine = Number(
+						row.original.total ?? row.original.subtotal ?? row.original.quantity * unitPrice,
+					);
+					return (
+						<span className='text-sm font-semibold text-zinc-900 dark:text-zinc-100'>
+							{priceFormatWhitDecimals(totalLine)}
+						</span>
+					);
+				},
+				meta: { align: 'right' },
+				size: 120,
+			},
 		],
 		[],
 	);
