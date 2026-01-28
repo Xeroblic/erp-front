e6419f3e834382e0574e8d6263dc597e4eb33128
r4veeen <bas.rojasl@duocuc.cl>
2026-01-28 15:17:22 -0300

    cambio para administrar feat:[C[D[F[3~[B[C[1;5C[1;5C[1;5C[F[B[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C[1;5C y mejora en diseño de perfil

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,7 @@ export default defineConfig({
         },
     },
     build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
+        minify: 'esbuild',
         chunkSizeWarningLimit: 1500,
         rollupOptions: {
             output: {
